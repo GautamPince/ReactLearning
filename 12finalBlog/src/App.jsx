7eea6769501c776d5628293382df5335cc0f7774
GautamPince <ramgautam9128@gmail.com>
2025-07-28 11:06:21 +0530

    feat: integrate Tailwind CSS and set up Redux for authentication
    - Added Tailwind CSS and its Vite plugin to the project dependencies.
    - Implemented Redux for managing authentication state with login and logout actions.
    - Created Header and Footer components for layout structure.
    - Updated App component to handle user authentication state and loading.
    - Configured Redux store to include auth slice.
    - Adjusted main entry point to wrap App with Redux Provider.

--- 12finalBlog/src/App.jsx
+++ 12finalBlog/src/App.jsx
@@ -1,18 +1,43 @@
-import React, { } from 'react'
-// import conf from './conf/conf.js'
+import React, { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+
 import './App.css'
+
+import authServices from './appwrite/auth';
+import { login, logout } from './store/authSlice';
+import { Footer, Header } from './components';
 
 function App() {
 
-  console.log(import.meta.env.VITE_APPWRITE_URL);
-  // console.log(conf.appwriteUrl);
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    authServices.getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }))
+        }
+        else {
+          dispatch(logout())
+        }
+      })
+      .finally(() => setLoading(false))
+  },)
 
 
-  return (
-    <>
-      <h1>A blog with Appwrite</h1>
-    </>
-  )
+  // return !loading ? ():();
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+          TODO: {/* <Outlet /> */}
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
